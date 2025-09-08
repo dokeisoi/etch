0833c1a112baf32791298e1fd41791a2f0529275
Tergel Tuvshinjargal <ttergel@protonmail.com>
2025-09-08 22:48:08 +0800

    feat: implement function to add grid of squares

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 function appendDiv(number) {
     for (i = 0; i < number; i++) {
         const div = document.createElement('div');
+
+        div.classList.add(`blue${i}`)
+        div.classList.add('upper');
+
         document.querySelector('.container').append(div);
     }
 }
+
+function appendAnotherDiv(number) {
+    for (i = 0; i < number; i++) {        
+        for ( j = 0; j < number; j++) {
+            const div = document.createElement('div');
+
+            div.classList.add('lower');
+
+            document.querySelector(`.blue${i}`).append(div);
+        }
+    }
+}
+
+appendDiv(50);
+appendAnotherDiv(50);
